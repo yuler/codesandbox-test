c9e81cb9f7262a049a2f25d8283e1312c7b8eef2
yuler <joyful4yu@gmail.com>
2020-05-10 21:08:36 +0800

    Use micro instead of koa

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const Koa = require('koa')
-const app = new Koa()
-
-app.use(async function(ctx) {
-  ctx.body = 'Hello World'
-})
-
-app.listen(3000)
+module.exports = (req, res) => {
+  res.end('Welcome to Micro')
+}
